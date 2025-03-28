8147007b57be3770a566d55f5293c13ab305bd3a
Mariam <AlSiyabiM1@cardiff.ac.uk>
2025-03-28 14:44:40 +0000

    shopping list base templates and scripts (no functionality)

---
+++ web_app/shoppingList.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
+import os
+
+
+app = Flask(__name__, template_folder="templates")
+app.secret_key = os.urandom(24)
+
+
+
+# Dashboard Route
+
+# Login Route
+
+# Register Route
+
+# Logout Route
+
+# Inventory Interface Route
+
+# Shopping List Interface Route
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

380e58415d237116692ff2fa0b0b7e4d0ac7dfe7
Mariam <AlSiyabiM1@cardiff.ac.uk>
2025-04-01 13:49:28 +0100

    shoppinglist.py

--- web_app/shoppingList.py
+++ web_app/shoppingList.py
@@ -1,24 +1,5 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for, session
-import os
+import mariadb
 
-
-app = Flask(__name__, template_folder="templates")
-app.secret_key = os.urandom(24)
-
-
-
-# Dashboard Route
-
-# Login Route
-
-# Register Route
-
-# Logout Route
-
-# Inventory Interface Route
-
-# Shopping List Interface Route
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+class database():
+    def __init__(self):
+        self.connection = self.connect()

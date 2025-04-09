8aff80b8f1137ea3c97f948b1df19dc091c62e30
Mariam <AlSiyabiM1@cardiff.ac.uk>
2025-04-09 19:07:20 +0100

    get jwt token and retieve data from telemetry

--- web_app/app.py
+++ web_app/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for, session
 import os
+import paho.mqtt.client as mqtt
+import json
+import time
+import requests
 from inventory import inventory
 from notification import notification
 
@@ -9,15 +13,92 @@ app.secret_key = os.urandom(24)
 # notification class instance
 notif = notification()  
 
+def get_jwt_token():
+    login_url = "https://thingsboard.cs.cf.ac.uk/api/auth/login"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "username": "group01@cardiff.ac.uk",
+        "password": "group012025"
+    }
+
+    response = requests.post(login_url, json=data, headers=headers)
+
+    if response.status_code == 200:
+        print('Login successful')
+        token = response.json()['token']
+        print("JET token:", token)
+        return token
+    else:
+        print("Login failed: ", response.json())
+
+def get_telemetry(token, device_id):
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+     }
+
+    url = f"https://thingsboard.cs.cf.ac.uk/api/plugins/telemetry/DEVICE/{device_id}/values/timeseries?keys=temperature,humidity"
+
+    # s = requests.Session()
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 401:
+        print("Token expired, refreshing...")
+        new_token = get_jwt_token()
+        if new_token:
+            return get_telemetry(new_token, device_id)
+        else:
+            return None
+    elif response.ok:
+        data = response.json()
+        return data
+    else:
+        print("Error: ", response.status_code, response.text)
+        return None
+
+
 # Dashboard Route
 @app.route('/')
 def index():
+    # JWT_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJncm91cDAxQGNhcmRpZmYuYWMudWsiLCJzY29wZXMiOlsiVEVOQU5UX0FETUlOIl0sInVzZXJJZCI6IjQ5ODQwYWQwLWQxOTQtMTFlZi05NjkzLWY3NWRhNjRlN2IzYSIsImVuYWJsZWQiOnRydWUsImlzUHVibGljIjpmYWxzZSwidGVuYW50SWQiOiJjY2E5MzhjMC1kMTkxLTExZWYtOTY5My1mNzVkYTY0ZTdiM2EiLCJjdXN0b21lcklkIjoiMTM4MTQwMDAtMWRkMi0xMWIyLTgwODAtODA4MDgwODA4MDgwIiwiaXNzIjoidGhpbmdzYm9hcmQuaW8iLCJpYXQiOjE3NDQyMDEwODYsImV4cCI6MTc0NDIxMDA4Nn0.BWM6GJX9qHUSLM8x21bAzVBkxmCNc1qAn94U-4ulUQ9h_D5f0LWVASdA_InfZOrZU5H3hA781Wubdrz81i_KsA"
+    device_id = "15b7a650-0b03-11f0-8ef6-c9c91908b9e2"
+
+    # headers = {
+    #     "Authorization": f"Bearer {JWT_TOKEN}",
+    #     "Content-Type": "application/json"
+    # }
+
+    # url = f"https://thingsboard.cs.cf.ac.uk/api/plugins/telemetry/DEVICE/{device_id}/values/timeseries?keys=temperature,humidity"
+
+    # # s = requests.Session()
+    # response = requests.get(url, headers=headers)
+
+    # if response.ok:
+    #     data = response.json()
+    #     print("Telemetry: ", data)
+    # else:
+    #     print("Error: ", response.status_code, response.text)
+
+    token = get_jwt_token()
+    data = get_telemetry(token, device_id)
+    temperature = humidity = None
+
+    if data:
+       temperature = float(data['temperature'][0]['value'])
+       humidity = float(data['humidity'][0]['value'])
+
     user_id = 2
 
     temp_url = "https://thingsboard.cs.cf.ac.uk/dashboard/9c597b10-0b04-11f0-8ef6-c9c91908b9e2?publicId=0d105160-0daa-11f0-8ef6-c9c91908b9e2" 
     humid_url = "https://thingsboard.cs.cf.ac.uk/dashboard/74d87180-0dbc-11f0-8ef6-c9c91908b9e2?publicId=0d105160-0daa-11f0-8ef6-c9c91908b9e2"
 
-    temp_humid = notif.temperature_humidity_notification(user_id, temp_url, humid_url)
+    # temperature = get_thingsboard_value("temperature")
+    # humidity = get_thingsboard_value("humidity")
+    # print(temperature, humidity)
+
+    notif.temperature_humidity_notification(user_id, temperature, humidity)
 
     notifications = notif.get_notifications(user_id)
 

38c1b765f6ff9a0f644d40a5fe78d49e3dc16780
Corey Taylor <coreyblaketaylor@hotmail.com>
2025-04-10 17:39:14 +0100

    Added AI Object Recognition Code

---
+++ web_app/barcode.py
@@ -0,0 +1,37 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+class barcode():
+    def __init__(self):
+        self.capture = cv2.VideoCapture(0)
+        self.number = None
+
+    def get_barcode(self):
+        return self.number
+    
+    def clear_barcode(self):
+        self.number = None
+
+    def decode_barcode(self):
+        self.number = None
+        self.capture = cv2.VideoCapture(0)
+        while True:
+            ret, frame = self.capture.read()
+            if not ret:
+                break
+            decoded = decode(frame)
+            if decoded:
+                self.number = decoded[0].data.decode('utf-8')
+            else:
+                # Encode the frame to JPEG
+                ret, buffer = cv2.imencode('.jpeg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+    def release_capture(self):
+        if self.capture.isOpened:
+            self.capture.release()
+
+    
+

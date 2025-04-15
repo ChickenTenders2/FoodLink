4d0c4286f9fafea9f9e31618206da2afa9c503f1
Franky <buckf@cardiff.ac.uk>
2025-04-15 05:15:28 +0100

    added report sorting

--- web_app/static/js/reports.js
+++ web_app/static/js/reports.js
@@ -14,7 +14,39 @@ async function get_reports() {
     }
 }
 
-function sort_reports() {}
+function sort_filter_reports() {
+    let reports = [];
+    const checked = document.getElementById("show_null_assign").checked;
+    const sort_by = document.getElementById("sort_by").value;
+    // Copies reports array
+    for (let report of window.reports) {
+        //If showing not assinged reports is unchecked, skip null values
+        if (checked || report[4] != null) {
+            reports.push(report);
+        }
+    }
+
+    // Sorts by error type with missing appearing first
+    if (sort_by == "error_type") {
+        reports.sort((a, b) => {
+            // If report error_type is missing assign value 0
+            error_value = (report) => report[2] == "missing" ? 0:1;
+            return error_value(a) - error_value(b);
+        });
+    } else if (sort_by == "date_asc") {
+        reports.sort((a, b) => {
+            date_value = (report) => new Date(report[3]);
+            return date_value(a) - date_value(b);
+        });
+    } else if (sort_by == "date_desc") {
+        reports.sort((a, b) => {
+            date_value = (report) => new Date(report[3]);
+            return date_value(b) - date_value(a);
+        });
+    }
+    console.log(reports);
+    fill_table(reports);
+}
 
 function fill_table(reports) {
     const table_body = document.getElementById("table_body");

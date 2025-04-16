4913cac375ad7f545873abf60b7cec904ec7be6a
Mariam <AlSiyabiM1@cardiff.ac.uk>
2025-04-16 20:38:39 +0100

    expiry notification function, checking notification duplicates function and inserting notification function

--- web_app/notification.py
+++ web_app/notification.py
@@ -1,5 +1,6 @@
 import mariadb
 import requests
+from datetime import datetime, timedelta
 
 class database():
     def __init__(self):
@@ -8,16 +9,16 @@ class database():
     # returns db connection
     def connect(self):
         return mariadb.connect(
-            host = "80.0.43.124",
+            host = "81.109.118.20",
             user = "FoodLink",
             password = "Pianoconclusiontown229!",
             database = "FoodLink"
-        )
+        ) # old ip: 80.0.43.124
 
 class notification(database):
     def get_notifications(self, user_id):
         cursor = self.connection.cursor()
-        query = "SELECT id, type, message, date_created, is_read, severity FROM notification WHERE user_id = %s;"
+        query = "SELECT id, type, message, date_created, is_read, severity FROM notification WHERE user_id = %s ORDER BY date_created DESC;"
         data = (user_id,)
         cursor.execute(query, data)
         notifications = cursor.fetchall()
@@ -47,21 +48,100 @@ class notification(database):
         
         if alerts_enabled:
             if temperature is not None:
+                temperature = round(temperature, 2)
+                notif_type = 'temperature'
                 if temperature < min_temperature:
-                    query = "INSERT INTO notification (user_id, type, message, date_created, is_read, severity) VALUES (%s, 'temperature', %s, NOW(), 0, 'warning')"
-                    data = (user_id, f"Low temperature detected: {temperature}°C")
-                    self.connection.commit()
-                    cursor.execute(query, data)
+                    message = f"Low temperature detected: {temperature}°C"
+                    is_exists = self.notification_exists(user_id, notif_type, message)
+                    if is_exists == 0:
+                        self.insert_notification(user_id, notif_type, message, severity)
                 elif temperature > max_temperature:
-                    query = "INSERT INTO notification (user_id, type, message, date_created, is_read, severity) VALUES (%s, 'temperature', %s, NOW(), 0, 'critical')"
-                    data = (user_id, f"High temperature detected: {temperature}°C")
-                    self.connection.commit()
-                    cursor.execute(query, data)
+                    message = f"High temperature detected: {temperature}°C"
+                    is_exists = self.notification_exists(user_id, notif_type, message)
+                    if is_exists == 0:
+                        self.insert_notification(user_id, notif_type, message, severity)
                 
             if humidity is not None:
+                humidity = round(humidity, 2)
                 if humidity > max_humidity:
-                    query = "INSERT INTO notification (user_id, type, message, date_created, is_read, severity) VALUES (%s, 'humidity', %s, NOW(), 0, 'warning')"
-                    data = (user_id, f"High humidity detected: {humidity}%")
-                    self.connection.commit()
-                    cursor.execute(query, data)
+                    notif_type = 'humidity'
+                    message = f"High humidity detected: {humidity}%"
+                    is_exists = self.notification_exists(user_id, notif_type, message)
+                    if is_exists == 0:
+                        self.insert_notification(user_id, notif_type, message, severity)
+        cursor.close()
+
+    def expiry_notification(self, user_id):
+        cursor = self.connection.cursor()
+        # query = "SELECT expiry_date FROM inventory WHERE user_id = %s"
+        query = "SELECT inv.id, i.id, i.name, expiry_date FROM FoodLink.inventory inv JOIN FoodLink.item i ON (inv.item_id = i.id) WHERE inv.user_id = %s;"
+        data = (user_id,)
+        cursor.execute(query, data)
+        expiries = cursor.fetchall()
+
+        today = datetime.today().date()
+        notif_type = 'expiry'
+        message = ''
+        severity = ''
+
+        for item in expiries:
+            item_name = item[2]
+            expiry_date = item[3]
+
+            if isinstance(expiry_date, datetime):
+                expiry_date = expiry_date.date()
+            
+            daysLeft = (expiry_date - today).days
+
+            if daysLeft == 2:
+                message = f"{item_name} will expire in 2 days."
+                severity = 'info'
+            elif daysLeft == 1:
+                message = f"{item_name} will expire tomorrow!"
+                severity = 'warning'
+            elif daysLeft == 0:
+                message = f"{item_name} expires today!"
+                severity = 'critical'
+            elif daysLeft < 0:
+                message = f"{item_name} has expired!"
+                severity = 'critical'
+            # else:
+            #     continue
+            
+            is_exists = self.notification_exists(user_id, notif_type, message)
+            if is_exists == 0:
+                self.insert_notification(user_id, notif_type, message, severity)
+
+            # duplicate_query = "SELECT COUNT(*) FROM notification WHERE user_id = %s AND message = %s;"
+            # cursor.execute(duplicate_query, (user_id, message))
+            # if cursor.fetchone()[0] == 0:
+            #     insert_query = "INSERT INTO notification (user_id, type, message, date_created, is_read, severity) VALUES (%s, 'expiry', %s, NOW(), 0, %s)"
+            #     insert_data = (user_id, message, severity)
+            #     cursor.execute(insert_query, insert_data)
+            
+            # self.connection.commit()
+        cursor.close()
+
+    def notification_exists(self, user_id, notif_type, message):
+        cursor = self.connection.cursor()
+        query = "SELECT COUNT(*) FROM notification WHERE user_id = %s AND type = %s AND message = %s;"
+        data = (user_id, notif_type, message)
+        cursor.execute(query, data)
+        is_exists = cursor.fetchone()[0]
+        return is_exists
+
+    def insert_notification(self, user_id, notif_type, message, severity):
+        cursor = self.connection.cursor()
+        query = "INSERT INTO notification (user_id, type, message, date_created, is_read, severity) VALUES (%s, %s, %s, NOW(), 0, %s)"
+        data = (user_id, notif_type, message, severity)
+        cursor.execute(query, data)
+        self.connection.commit()
+        cursor.close()
+
+    def mark_read(self, notif_id):
+        cursor = self.connection.cursor()
+        query = "UPDATE notification SET is_read = 1 WHERE id = %s"
+        data = (notif_id,)
+        cursor.execute(query, data)
+        self.connection.commit()
         cursor.close()

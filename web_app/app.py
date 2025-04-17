d25eaaa4edad95785c0904e48989fc2e07c72321
Mariam <AlSiyabiM1@cardiff.ac.uk>
2025-04-17 13:36:36 +0100

    remove invetory code from notification branch

--- web_app/app.py
+++ web_app/app.py
@@ -98,46 +98,6 @@ def index():
 # Logout Route
 
 # Inventory Interface Route
-inv = inventory()
-
-@app.route('/inventory')
-def get_inventory():
-    user_id = 2
-    search_query = request.args.get('search')
-    sort_by = request.args.get('sort_by')
-
-    # searches for an item if query is provided otherwise gets all items
-    if search_query:
-        items = inv.search_items(user_id, search_query)
-    else:
-        items = inv.get_items(user_id)
-    
-    if sort_by in ['name', 'expiry']:
-        # sorts by name or expiry
-        items = sorted(items, key=lambda x: x[2] if sort_by == 'name' else x[6])
-
-    # formats each item as list for easier modification of date format
-    items = [list(i) for i in items]
-    # formats date for front end
-    for item in items:
-        item[6] = item[6].strftime('%Y-%m-%d')
-
-    return render_template("inventory.html", items=items, sort_by=sort_by)
-
-@app.route('/update_item', methods=['POST'])
-def update_item():
-    # gets variables needed to update item
-    inventory_id = request.form['inventory_id']
-    quantity = request.form['quantity']
-    expiry_date = request.form['expiry_date']
-    
-    try:
-        # Update the database with new quantity and expiry date
-        inv.update_item(inventory_id, quantity, expiry_date)
-        # returns response to js code
-        return jsonify({'success': True})
-    except Exception as e:
-        return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)

b83ead5218ded0f7fa7d8c8a929b6f95e0a1a8a0
Matheus Da Luz Larini <daluzlarinim@cardiff.ac.uk>
2025-05-04 06:35:06 +0000

    docstrings for some functions

--- web_app/app.py
+++ web_app/app.py
@@ -209,6 +209,7 @@ def index():
 
 @app.route('/admin/login', methods=['GET', 'POST'])
 def admin_login():
+    """Handles admin login authentication."""
     form = LoginForm()
     error = None
     
@@ -227,7 +228,7 @@ def admin_login():
 @app.route("/admin/add", methods=["GET", "POST"])
 @admin_only
 def AddAdmin():
-    # Must be advanced admin to add new admins
+    """Allows advanced admins only to add a new admin account."""
     if not current_user.advanced_privileges:
         flash("You are not authorized to add new admins.", "danger")
         return redirect(url_for("AdminDashboard"))
@@ -267,6 +268,7 @@ def AddAdmin():
 @app.route("/admin/update-password", methods=["GET", "POST"])
 @admin_only
 def AdminUpdatePassword():
+    """Allows an admin to update their password."""
     form = AdminPasswordForm()
     message = None
 
@@ -289,6 +291,7 @@ def AdminUpdatePassword():
 @app.route("/admin/dashboard")
 @admin_only
 def AdminDashboard():
+    """Displays the admin dashboard."""
     return render_template("admin_dashboard.html")
 
 # Logout route
